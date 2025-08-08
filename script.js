d8fa14f0773f6c4e24c7c0865be34b7c57282abc
blackdragon <juliorodriguez2498@gmail.com>
2025-08-08 10:36:29 -0600

    version final del api

--- script.js
+++ script.js
@@ -1,41 +1,51 @@
+    // para sacar la fecha del dia que estan consultando...
     const fechaHoy = new Date();
     const mostrarFecha = document.getElementById('mostrarFecha');
     mostrarFecha.innerHTML = fechaHoy.toLocaleDateString();
 
 async function llamarAPI(){ //lo hago con async porque RapidAPI me lo da con un await al generarlo 
-                              // de una vez con el fetch y no me quiero complicar la vida cambiando de forma...
-    const url = 'https://rottentomato.p.rapidapi.com/search?search-term=kin';
+                            // de una vez con el fetch y no me quiero complicar la vida cambiando de forma...
+        const buscarPelicula = document.getElementById('busqueda').value;
+        const url = `https://rottentomato.p.rapidapi.com/search?search-term=${encodeURIComponent(buscarPelicula)}`; 
+                                                        //encodeURIComponent para evitar que de error al buscar algo con una tilde o espacios
         const options = {
 	    method: 'GET',
 	    headers: {
-			// se agrega la llave proporcionada por RapidAPI
-			// se agrega el host
+			'x-rapidapi-key': '',
+            'x-rapidapi-host': ''
 	    }
     };
+
     try {
         const respuesta = await fetch(url, options);
         const data = await respuesta.json();
         console.log(data);
 
         const contenedor = document.getElementById("contenedor-peliculas");
-        contenedor.innerHTML="";
-        const peliculas = data.movies_shows.slice(0,5);
+        contenedor.innerHTML='';
 
-        peliculas.forEach(peli => {
-            const poster = peli.thumbnail || "https://via.placeholder.com/300x450?text=Sin+imagen";
-            const titulo = peli.title || "Sin titulo";
-            const publicada = peli.year || "Año desconocido";
+        const items = (data?.movies_shows).slice(0,5);
+
+        items.forEach(pelicula => {
             
-            const carta = `<div class="col-md-4-col-lg-3mb-4">
+            const poster = pelicula.posterImageUrl || 'https://via.placeholder.com/300x450?text=Sin+Imagen';
+            const titulo = pelicula.title || "Sin titulo";
+            const publicada = pelicula.releaseYear || "Año desconocido";
+            const puntuacion = pelicula.rottenTomatoes?.audienceScore || "Puntuacion desconocida";
+
+            const carta = `
+                            <div class="col-md-4 col-lg-3 mb-4">
                                 <div class="card h-100">
-                                    <img src="$(poster)" class="card-img-top" alt="$(titulo)">
-                                        <div class="card-body">
-                                            <h5 class="card-tittle">${titulo}</h5>
-                                            <p class="card-text"><strong>Año:</strong> ${publicada}</p>
-                                        </div>    
+                                    <img src="${poster}" class="card-img-top" alt="${titulo}">
+                                    <div class="card-body">
+                                        <h5 class="card-title">${titulo}</h5>
+                                        <p class="card-text"><strong>Año:</strong> ${publicada}</p>
+                                        <p class="card-text">Puntuacion: ${puntuacion} %</p>
+                                    </div>
                                 </div>
-                          </div>
+                            </div>
                           `;
+
             contenedor.innerHTML += carta;
         });
 
@@ -43,7 +53,3 @@ async function llamarAPI(){ //lo hago con async porque RapidAPI me lo da con un 
         console.error("Error al cargar...",error);
     }
 }
-
-
-llamarAPI();
-
